16f484a4adc4723a5d63f2850774a6d94a62c393
tonyp <palmerantony@gmail.com>
2021-03-03 14:31:25 +0000

    Make the odd-even game loop

--- odd-or-even/OddOrEven.py
+++ odd-or-even/OddOrEven.py
@@ -1,12 +1,20 @@
 # A simple programme to tell a user if a number is odd or even
 print("Hi! What number are you thinking of today?")
-
-# input() is a built in function to accept console input
 numberAsString = input()
-print("Great, your number was: " + numberAsString)
 
-number = int(numberAsString)
-if number % 2 == 0:
-    print("Your number is EVEN!")
-else: 
-    print("Your number is ODD!")
+shouldContinue = "Y"
+
+while shouldContinue != "N":
+# input() is a built in function to accept console input
+    print("Great, your number was: " + numberAsString)
+    number = int(numberAsString)
+    if number % 2 == 0:
+        print("Your number is EVEN!")
+    else: 
+        print("Your number is ODD!")
+
+    print("To continue, enter another number. Otherwise type N to exit")
+    shouldContinue = input()
+    numberAsString = shouldContinue
+
+print("Thanks for playing :)")
