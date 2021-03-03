585a95f3844456cfa5a4a1db4766c09b3497419d
tonyp <palmerantony@gmail.com>
2021-03-03 15:24:17 +0000

    Add a foolish programme that does stupid abbreviation

---
+++ internal-number-abbreviator/InternalNumberAbbreviator.py
@@ -0,0 +1,12 @@
+# A tiny programme that asks a user for a word and then returns it with the first and last letter surrounding the number of removed letters
+# for example, 'internationalisation' becomes 'i18n'
+
+print("Please enter a word and I'll give you a stupid abbreviation for it")
+word = input().upper()
+firstLetter = word[0]
+lastLetter = word[len(word)-1]
+num = len(word) - 2
+
+result = firstLetter + str(num) + lastLetter
+
+print("The stupid abbreviation is: " + result)
